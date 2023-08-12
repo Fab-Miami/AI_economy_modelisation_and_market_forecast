cb12241d3cc0cf455c89a71cc01e1c4577c1ea28
FabMia <fabricecolozzi@gmail.com>
2023-08-12 11:59:56 -0400

    new loading

--- _MODEL_1/use_specific_model.py
+++ _MODEL_1/use_specific_model.py
@@ -1,48 +1,36 @@
 import os
-
-from rich.markdown import Markdown
 from rich.console import Console
 from rich.table import Table
+from tensorflow.keras.models import load_model
+from tensorflow.keras.optimizers import Adam
+
 console = Console()
 
-from keras.models import load_model
-from keras.optimizers import Adam
-
-
-# List all files in the models directory
+# List all directories in the models directory
 model_path = '/Users/c/Desktop/AI/proto1/models'
+models = [f for f in os.listdir(model_path) if os.path.isdir(os.path.join(model_path, f))]
 
-models = [f for f in os.listdir(model_path) if f.endswith('.keras')]
 # Print models with numbers
-
-
 table = Table(show_header=True, header_style="bold magenta")
 table.add_column("ID", style="dim", width=10)
 table.add_column("Model")
-table.add_column("Size (KB)")
 
-for i, model_file in enumerate(models, 1):
-    file_size = os.path.getsize(os.path.join(model_path, model_file)) / 1024  # Size in KB
-    table.add_row(f"{i}", f"{model_file}", f"{file_size}")
+for i, model_dir in enumerate(models, 1):
+    table.add_row(f"{i}", f"{model_dir}")
 
 console.print(table)
 
 # Ask the user to choose a model
 console.print("Choose a model number to load:", style="bold yellow")
 choice = int(input()) - 1
-# choice = int(input("Choose a model number to load: ")) - 1
 
 # Check if the choice is valid
 if 0 <= choice < len(models):
-    model_file_to_load = os.path.join(model_path, models[choice])
-
-
-    model = load_model(model_file_to_load, custom_objects={'Adam': Adam})
-
-    # model = load_model(model_file_to_load)
+    model_dir_to_load = os.path.join(model_path, models[choice])
+    model = load_model(model_dir_to_load, custom_objects={'Adam': Adam})
     print(f"Model {models[choice]} has been loaded!")
-    # print("Model Summary:")
-    # model.summary()  # Print a summary of the model's architecture
+    print("\n\nModel Summary:")
+    model.summary()  # Print a summary of the model's architecture
 else:
     print("Invalid choice!")
 
